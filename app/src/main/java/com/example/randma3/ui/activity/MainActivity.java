41b1786a746577fed8a0d97334290f590dc256d6
aiazbekov33 <89203588+aiazbekov33@users.noreply.github.com>
2021-11-17 23:10:20 +0600

    bottomNav_toolBar_Navigation

--- app/src/main/java/com/example/randma3/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/randma3/ui/activity/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.randma3.R;
+import com.example.randma3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
--- app/src/main/java/com/example/randma3/ui/fragments/character/CharacterFragment.java
+++ app/src/main/java/com/example/randma3/ui/fragments/character/CharacterFragment.java
@@ -2,6 +2,8 @@ package com.example.randma3.ui.fragments.character;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -23,5 +25,10 @@ public class CharacterFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentCharacterBinding.inflate(getLayoutInflater(),container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
