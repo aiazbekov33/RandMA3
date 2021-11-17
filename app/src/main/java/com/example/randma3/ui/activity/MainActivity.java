2341442343cb7b4ca2718dfe1c75b9890faa33ec
aiazbekov33 <89203588+aiazbekov33@users.noreply.github.com>
2021-11-17 23:31:01 +0600

    Подключение bottom_toolbar_appBar_configuration

--- app/src/main/java/com/example/randma3/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/randma3/ui/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.randma3.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -16,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setupNavigation();
     }
+    private void setupNavigation() {
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.characterFragment, R.id.episodeFragment, R.id.locationFragment
+        ).build();
+        NavigationUI.setupWithNavController(binding.toolBar, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(binding.bottomNav, navController);
+    }
+
 }
